dd41a8d710d2da150e6d65d5527234c09951d9b8
luarodriguess <luarodrigues1996@gmail.com>
2025-06-24 16:47:40 -0300

    feat: add slogam

--- src/app/packages/components/hero-section/hero-section.tsx
+++ src/app/packages/components/hero-section/hero-section.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image';
 import styles from './HeroSection.module.scss';
+import { motion } from 'framer-motion';
 
 export default function HeroSection() {
   return (
@@ -13,20 +14,37 @@ export default function HeroSection() {
         priority
         className={styles.bgImage}
       />
+
       <div className={styles.overlay}>
-        <h1>
-          INOVAÇÃO EM TECNOLOGIA <br />
-          PARA O FUTURO
-        </h1>
-        <p>Transformando ideias em soluções digitais</p>
-        <a
+        <motion.h1
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          Innovate. Develop. Transform.
+        </motion.h1>
+
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.2, delay: 0.2 }}
+        >
+          Transformando ideias em soluções digitais
+        </motion.p>
+
+        <motion.a
           href="https://api.whatsapp.com/send?phone=5551997791996"
           target="_blank"
           rel="noopener noreferrer"
           className={styles.button}
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.98 }}
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.8, delay: 0.4 }}
         >
           SAIBA MAIS
-        </a>
+        </motion.a>
       </div>
     </section>
   );
